ee6c7da86ee08d16ad839c5bb081814cefd6d930
PavelDermichev1998 <pdermichev@gmail.com>
2021-10-10 15:58:17 +0300

    Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,38 @@
 import React from 'react';
 import './App.css';
-
+import Accordion from './components/Accordion/Accordion';
+import {Rating} from "./components/Rating/Rating";
 
 
 // function declaration
 function App() {
+    console.log("App rendering")
     // return JSX {
     return (
         <div>
-            this is Ap
-            <Rating />
-            <Accordion />
+            <PageTitle title = {"This is App"}/>
+            <PageTitle title = {"My friends"}/>
+            <Rating value = {3}/>
+            <Accordion title = {"Title"}/>
+            <Accordion title = {"Menu"}/>
+            <Rating value = {4}/>
+            <Rating value = {2}/>
+            <Rating value = {1}/>
+            <Rating value = {0}/>
+            <Rating value = {5}/>
         </div>
     );
 }
 
-function Rating() {
-    // return JSX {
+function PageTitle(props: any) {
+    console.log("PageTitle rendering")
     return (
-        <div>
-            <Star />
-            <Star />
-            <Star />
-            <Star />
-            <Star />
-        </div>
+        <h1>
+            {props.title}
+        </h1>
     );
 }
 
-function Accordion() {
-    return (
-        <div>
-            <h3>Menu</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-        </div>
-    );
-}
-
-function Star() {
-    return (
-        <div>
-            star
-        </div>
-    );
-}
 
 export default App;
 
