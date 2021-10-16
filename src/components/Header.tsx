084a8d74cb8d62e595459f0f8f999ac789cfdaae
PavelDermichev1998 <pdermichev@gmail.com>
2021-10-16 13:18:20 +0300

    Component - css

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function Header() {
+    return (
+
+        <header className='header'>
+            <img
+                src="https://cdn.shopify.com/shopifycloud/hatchful_web_two/bundles/aff2c7c41798a9e8d510293d676b1308.png"
+                alt=""/>
+        </header>
+
+    );
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export function Profile() {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://www.imgacademy.com/themes/custom/imgacademy/images/helpbox-contact.jpg'/>
+            </div>
+            <div>
+                ava + discription
+            </div>
+            <div>
+                my post
+                <div>
+                    new post
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
