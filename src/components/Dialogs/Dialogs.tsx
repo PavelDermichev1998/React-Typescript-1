dc56dfe1346677fb66a6a1435872192243e88473
PavelDermichev1998 <pdermichev@gmail.com>
2021-10-18 22:54:55 +0300

    css Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,11 +2,31 @@ import React from 'react';
 import s from './Dialogs.module.css'
 
 
-
-export function Dialogs () {
+export function Dialogs() {
     return (
-      <div>
-          Dialogs
-      </div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dima
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Lena
+                </div>
+                <div className={s.dialog}>
+                    Vit
+                </div>
+                <div className={s.dialog}>
+                    Pasha
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>It asd</div>
+                <div className={s.message}>Yes</div>
+            </div>
+        </div>
     );
 }
