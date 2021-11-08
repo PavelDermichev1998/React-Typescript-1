c4c3c6dfb78e5f95d7adef34027b200667ca0fe0
PavelDermichev1998 <pdermichev@gmail.com>
2021-11-08 22:48:57 +0300

    31 React.createRef()

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -14,15 +14,21 @@ export function MyPosts(props: MyPostsPropsType) {
     let postsElements = props.posts
         .map(p => <Post message={p.message} likesCount={p.likesCount}/>)
 
+    let newPostElement: any = React.createRef();
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text)
+    }
+
     return (
         <div className={s.postsBlock}>
             <h3>My post</h3>
             <div>
                 <div>
-                    <textarea/>
+                    <textarea ref={newPostElement}/>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
